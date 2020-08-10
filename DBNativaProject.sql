1f4c3fcd78f5c4f893049dcfc17907f5bcee1b35
Bgomez-2015482 <bgomez-2015482@kinal.edu.gt>
2020-08-10 13:10:39 -0600

    Adding products table on DB, adding menuPrincipalController and fixed Principal class

--- DBNativaProject.sql
+++ DBNativaProject.sql
@@ -1,2 +1,6 @@
 DROP DATABASE IF EXISTS dbnativaproject;
 CREATE DATABASE dbnativaproject;
+
+CREATE TABLE Productos(
+	codigoProducto int not null auto_increment
+);
--- NativaProject/src/org/nativa/sistema/Principal.java
+++ NativaProject/src/org/nativa/sistema/Principal.java
@@ -1,12 +1,14 @@
 package org.nativa.sistema;
 
+import java.io.InputStream;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.nativa.controller.MenuPrincipalController;
 
 public class Principal extends Application{
     private final String PAQUETE_VISTA = "/org/nativa/view/";
@@ -18,7 +20,34 @@ public class Principal extends Application{
         this.escenarioPrincipal = escenarioPrincipal;
         escenarioPrincipal.setTitle("Nativa Maneger");
         menuPrincipal();
+        escenarioPrincipal.show();
     }
     
-    public void menuPrincipal(){}
+    public void menuPrincipal(){
+        try{
+            MenuPrincipalController menuPrincipal = (MenuPrincipalController)cambiarEscena("MenuPrincipalView.fxml", 600, 400);
+            menuPrincipal.setEscenarioPrincipal(this);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public Initializable cambiarEscena(String fxml, int ancho, int alto) throws Exception{
+        Initializable resultado = null;
+        FXMLLoader cargadorFXML = new FXMLLoader();
+        InputStream archivo = Principal.class.getResourceAsStream(PAQUETE_VISTA+fxml);
+        cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
+        cargadorFXML.setLocation(Principal.class.getResource(PAQUETE_VISTA+fxml));
+        escena = new Scene((AnchorPane)cargadorFXML.load(archivo), ancho, alto);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.sizeToScene();
+        resultado = (Initializable)cargadorFXML.getController();
+        
+        return resultado;
+    }
+    
+    public static void main(String[]args){
+    launch(args);
+    }
+    
 }
